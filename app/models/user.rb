d9aa9432a315d980a0d0347bdacc241d9c8e1471
M-Katayama-10 <nokmtinok@gmail.com>
2021-04-29 01:54:13 +0900

    バリデーション修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,17 +7,21 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :birthday
-  end
-  with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."} do
-    validates :first_name
-    validates :last_name
-  end
-  with_options presence: true, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."} do
-    validates :first_name_kana
-    validates :last_name_kana
-  end
-  with_options presence: true, format: {with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."} do
-    validates :password
-    validates :password_confirmation
+
+    with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."} do
+      validates :first_name
+      validates :last_name
+    end
+
+    with_options format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."} do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
+
+    with_options format: {with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."} do
+      validates :password
+      validates :password_confirmation
+    end
+    
   end
 end
