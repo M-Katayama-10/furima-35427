ba1093cea210750ed049566df7f196310157c1a2
M-Katayama-10 <nokmtinok@gmail.com>
2021-04-29 20:39:06 +0900

    validation追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,18 +4,22 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :description
-    validates :category_id
-    validates :condition_id
-    validates :fee_id
-    validates :area_id
-    validates :days_id
     validates :price, format: {with: /\A[0-9]+\z/}, numericality: { in: 300..9999999 }
+
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_id
+      validates :fee_id
+      validates :area_id
+      validates :days_id
+    end
+    
   end
 
   belongs_to :user
   has_one_attached :image
 
-  
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   
   belongs_to :category
