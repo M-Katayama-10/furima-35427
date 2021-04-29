1ea91d934449d654f15a5750ad927152e852cd94
M-Katayama-10 <nokmtinok@gmail.com>
2021-04-29 19:53:49 +0900

    ActiveHash導入、バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,27 @@
 class Item < ApplicationRecord
 
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :fee_id
+    validates :area_id
+    validates :days_id
+    validates :price, format: {with: /\A[0-9]+\z/}, numericality: { in: 300..9999999 }
+  end
+
   belongs_to :user
   has_one_attached :image
+
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :fee
+  belongs_to :area
+  belongs_to :days
+
 end
