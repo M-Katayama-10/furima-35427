066a47e623f9848f30cbaaa851866cd8eba8a8cc
M-Katayama-10 <nokmtinok@gmail.com>
2021-04-30 17:46:44 +0900

    before_action、factories修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
     fee_id               { 2 }
     area_id              { 2 }
     days_id              { 2 }
-    price                { '50000' }
+    price                { 50000 }
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
